c47520146edd1560f4303a8a687034298638d706
frkri <57220519+frkri@users.noreply.github.com>
2024-09-27 09:40:29 +0000

    Add font

--- link-base/tailwind.config.js
+++ link-base/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const plugin = require('tailwindcss/plugin');
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		fontFamily: {
+			sans: ['Geist', 'sans-serif']
+		},
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+		plugin(function ({ addVariant }) {
+			addVariant('hocus', ['&:hover', '&:focus']);
+		})
+	]
 };
