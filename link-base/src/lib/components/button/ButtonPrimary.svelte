7deb43f2b88c00b3fa68a59d7907e57a27659d18
frkri <57220519+frkri@users.noreply.github.com>
2024-09-28 21:14:56 +0000

    Add button compoentes

---
+++ link-base/src/lib/components/button/ButtonPrimary.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { Button, type ButtonProps } from 'bits-ui';
+
+	let {
+		children,
+		...props
+	}: {
+		children: Snippet;
+	} & ButtonProps = $props();
+</script>
+
+<Button.Root
+	class="flex min-h-12 min-w-24 flex-row items-center justify-between gap-2 rounded border border-slate-100 bg-slate-100 p-2 text-slate-900 outline-none transition hocus:bg-opacity-0 hocus:text-slate-100"
+	{...props}
+>
+	{@render children()}
+</Button.Root>
