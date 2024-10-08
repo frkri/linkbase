6f654af915980f48a47fb35873fe8891e4f677e6
frkri <57220519+frkri@users.noreply.github.com>
2024-10-08 11:44:35 +0000

    Change sizing and bg color

--- linkbase/src/lib/components/nav/Header.svelte
+++ linkbase/src/lib/components/nav/Header.svelte
@@ -19,22 +19,23 @@
 
 <div bind:this={divElement} class="pointer-events-none sticky top-[-1px] h-28 pt-2">
 	<nav
-		class="pointer-events-auto flex h-12 w-full flex-row items-center justify-between rounded border border-slate-500 border-opacity-0 bg-stone-100 bg-opacity-40 px-2 backdrop-blur-sm dark:bg-stone-900 dark:bg-opacity-40"
+		class="pointer-events-auto flex h-12 w-full flex-row items-center justify-between rounded bg-stone-100 bg-opacity-40 px-2 backdrop-blur-sm dark:bg-stone-900 dark:bg-opacity-40"
 		class:!bg-opacity-0={!isPinned}
 		class:!border-opacity-30={isPinned}
+		class:drop-shadow-xl={isPinned}
 		class:min-h-28={!isPinned}
 	>
 		<button
-			class="flex flex-row place-items-center gap-4 justify-self-center rounded-lg bg-stone-500 bg-opacity-0 p-1 font-bold transition hocus:bg-opacity-40 md:gap-8"
+			class="hocus:bg-opacity-20 flex flex-row place-items-center gap-4 justify-self-center rounded-lg bg-stone-500 bg-opacity-0 p-1 font-bold transition md:gap-8"
 			onclick={() => scrollTo({ behavior: 'smooth', top: 0 })}
 		>
 			<Favicon
-				class="max-h-[45px] max-w-[45px] md:max-h-[60px] md:max-w-[60px]"
-				height={isPinned ? 30 : 60}
-				width={isPinned ? 30 : 60}
+				class="max-h-[30px] max-w-[30px] md:max-h-[50px] md:max-w-[50px]"
+				height={isPinned ? 30 : 50}
+				width={isPinned ? 30 : 50}
 			/>
 			{#if !isPinned}
-				<span class="text-4xl md:text-6xl" in:fade={{ duration: 200 }}>linkbase</span>
+				<span class="text-2xl md:text-4xl" in:fade={{ duration: 200 }}>linkbase</span>
 			{/if}
 		</button>
 		<Avatar />
