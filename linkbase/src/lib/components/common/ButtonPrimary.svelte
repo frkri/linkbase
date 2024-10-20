ac2486abc0c3584977e725df589aec1eed095d2e
frkri <57220519+frkri@users.noreply.github.com>
2024-10-20 11:54:51 +0000

    Switch to default buttons

--- linkbase/src/lib/components/common/ButtonPrimary.svelte
+++ linkbase/src/lib/components/common/ButtonPrimary.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import type { ComponentType, Snippet } from 'svelte';
 
-	import { Button, type ButtonProps } from 'bits-ui';
 	import { type Icon } from 'lucide-svelte';
+	import { type HTMLButtonAttributes } from 'svelte/elements';
 
 	let {
 		children,
@@ -13,11 +13,11 @@
 		children?: Snippet;
 		content?: string;
 		icon?: ComponentType<Icon>;
-	} & ButtonProps = $props();
+	} & HTMLButtonAttributes = $props();
 </script>
 
-<Button.Root
-	class="group/button flex min-h-10 min-w-10 flex-row items-center justify-center gap-2 rounded border border-slate-900 bg-stone-900 stroke-slate-700 p-2 text-sm text-stone-100 transition hocus:bg-stone-100 hocus:text-stone-900 dark:border-stone-100  dark:bg-stone-100  dark:stroke-slate-100 dark:text-slate-900  dark:hocus:bg-stone-900 dark:hocus:text-stone-100"
+<button
+	class="group/button flex min-h-10 min-w-10 flex-row items-center justify-center gap-2 rounded border border-slate-900 bg-stone-900 stroke-slate-700 p-2 text-sm text-stone-100 transition hocus:bg-stone-100 hocus:text-stone-900 dark:border-stone-100 dark:bg-stone-100 dark:stroke-slate-100 dark:text-slate-900 dark:hocus:bg-stone-900 dark:hocus:text-stone-100"
 	{...props}
 >
 	{#if children}
@@ -25,7 +25,7 @@
 	{:else}
 		{@render defaultContent(content, icon)}
 	{/if}
-</Button.Root>
+</button>
 
 {#snippet defaultContent(content: string, Icon?: ComponentType<Icon>)}
 	<div class="flex flex-row items-center justify-center gap-1.5 md:min-w-24">
--- linkbase/src/lib/components/common/ButtonSecondary.svelte
+++ linkbase/src/lib/components/common/ButtonSecondary.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import type { ComponentType, Snippet } from 'svelte';
 
-	import { Button, type ButtonProps } from 'bits-ui';
 	import { type Icon } from 'lucide-svelte';
+	import { type HTMLButtonAttributes } from 'svelte/elements';
 
 	let {
 		children,
@@ -13,11 +13,11 @@
 		children?: Snippet;
 		content?: string;
 		icon?: ComponentType<Icon>;
-	} & ButtonProps = $props();
+	} & HTMLButtonAttributes = $props();
 </script>
 
-<Button.Root
-	class="group/button flex min-h-10 min-w-10 flex-row	items-center justify-center gap-2 rounded border border-slate-900 bg-stone-400 bg-opacity-0 stroke-slate-700 p-2 text-sm text-stone-800 transition hocus:bg-opacity-15 dark:border-slate-100 dark:bg-stone-300 dark:bg-opacity-0 dark:stroke-slate-100 dark:text-slate-100"
+<button
+	class="group/button flex min-h-10 min-w-10 flex-row items-center justify-center gap-2 rounded border border-slate-900 bg-stone-400 bg-opacity-0 stroke-slate-700 p-2 text-sm text-stone-800 transition hocus:bg-opacity-15 dark:border-slate-100 dark:bg-stone-300 dark:bg-opacity-0 dark:stroke-slate-100 dark:text-slate-100"
 	{...props}
 >
 	{#if children}
@@ -25,7 +25,7 @@
 	{:else}
 		{@render defaultContent(content, icon)}
 	{/if}
-</Button.Root>
+</button>
 
 {#snippet defaultContent(content: string, Icon?: ComponentType<Icon>)}
 	<div class="flex flex-row items-center justify-center gap-1.5 md:min-w-24">
