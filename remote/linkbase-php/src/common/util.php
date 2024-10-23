e2f381e7cc38f2db8d8cf44e6c821f2ccce86a0d
frkri <57220519+frkri@users.noreply.github.com>
2024-10-23 17:56:26 +0200

    Update scrape timeout

--- remote/linkbase-php/src/common/util.php
+++ remote/linkbase-php/src/common/util.php
@@ -2,7 +2,7 @@
 function fetchContentWithCurl($url)
 {
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 120);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_MAXREDIRS, 3);
     curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
